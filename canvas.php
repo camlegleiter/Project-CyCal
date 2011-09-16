f57f809b9e8431c267767f92f119c0cc50a5a8b1
Justin Derby <justderb@iastate.edu>
2011-09-16 14:11:02 -0500

    Cleaned up files and made them more organized

--- canvas.php
+++ canvas.php
@@ -1,60 +1,72 @@
 <html>
 <head>
-
-<body style="background-image: url('img/cool-background_017.jpg')">
-<?php
-require("includes/connect.php");
-?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Canvas - CyCal</title>
 
 
-<h1 align="center" style="padding:0px; margin:0px;" class="auto-style1">Coming Soon</h1>
-<div style="margin-bottom: 3px; padding: 3px;"></div>
-<div style="clear:both; height:200px; margin:0px; padding:0px;"></div>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/canvas.css" rel="stylesheet" type="text/css">
+<link href="css/mainstyle.css" rel="stylesheet" type="text/css">
+
+
+
 </head>
 
-<?php
-?>
-<table align="center">
-<form method='POST'>
-<tr><td><strong><span class="auto-style1">Username:</span></strong></td> 
-<td><input type='text' name='user'></td>
-<td><input type='submit' name='submit' value='Log In'></td></tr>
-<tr><td class="auto-style1"><strong>Password:</strong></td> 
-<td><input type='password' name='pass'></td>
-<td><input type='submit' name='register' value='Register'></td>
-</tr>
-</form>
-</table>
+<body>
+<h1>h1 Header</h1>
+<h2>h2 Header</h2>
+<h3>h3 Header</h3>
+<h4>h4 Header</h4>
+<h5>h5 Header</h5>
+<h6>h6 Header</h6>
+<p>This is a p tag <a href="#">Hyperlink</a></p>
+<p>Line</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 
-<?php
-if($_POST['submit']){
-$user = mysql_real_escape_string($_POST['user']);
-$pass = mysql_real_escape_string($_POST['pass']);
-$extract = mysql_query("SELECT * FROM users WHERE username='".$user."' AND password='".md5($pass)."'");
-$numrows = mysql_num_rows($extract);
-if($user != ''){
-	if($numrows == 0){
-	echo "Access Denied! ".mysql_error();
-	echo "  |  SELECT * FROM users WHERE username='".$user."' AND password='".md5($pass)."'";
-	}
-	else{
-	echo"You are In!";
-//	session_start();
-//	$row = mysql_fetch_assoc($extract);
-//	$id = $row['id'];
-//	$write = $row['writeable'];
-//	$_SESSION['id']= $id;
-//		if($write){
-//		header("Location: add_delete.php");
-//		}
-//		else{
-//		header("Location: server_info.php");
-//		}
-	}
-}
-}
-//require("footer.tpl");
-?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,74 +1,101 @@
-<html>
-<head>
-<style type="text/css">
-table,th,td
-{
-
-}
-body
-{
-background-color:#b0c4de;
-)
-}
-.auto-style1 {
-	color: #FFFFFF;
-}
-</style>
-
-<body style="background-image: url('img/cool-background_017.jpg')">
 <?php
 require("includes/connect.php");
+
+if($_POST['submit']){
+	$user = mysql_real_escape_string($_POST['user']);
+	$pass = mysql_real_escape_string($_POST['pass']);
+	$extract = mysql_query("SELECT * FROM users WHERE username='".$user."' AND password='".md5($pass)."'");
+	$numrows = mysql_num_rows($extract);
+	if($user != ''){
+		if($numrows == 0){
+			echo "Access Denied! ".mysql_error();
+			echo "  |  SELECT * FROM users WHERE username='".$user."' AND password='".md5($pass)."'";
+		}
+		else{
+			echo"You are In!";
+		//	session_start();
+		//	$row = mysql_fetch_assoc($extract);
+		//	$id = $row['id'];
+		//	$write = $row['writeable'];
+		//	$_SESSION['id']= $id;
+	//		if($write){
+		//		header("Location: add_delete.php");
+	//		}
+	//		else
+	//      {
+		//		header("Location: server_info.php");
+	//		}
+		}
+	}
+}
+else if ($_POST['register'])
+{
+	//Send header redirect
+}
 ?>
 
 
-<h1 align="center" style="padding:0px; margin:0px;" class="auto-style1">Coming Soon</h1>
-<div style="margin-bottom: 3px; padding: 3px;"></div>
-<div style="clear:both; height:200px; margin:0px; padding:0px;"></div>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" /> 
+<html>
+<head>
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Canvas - CyCal</title>
+	
+	<link href="css/reset.css" rel="stylesheet" type="text/css">
+	<link href="css/mainstyle.css" rel="stylesheet" type="text/css">
+
 </head>
 
-<?php
-?>
-<table align="center">
-<form method='POST'>
-<tr><td><strong><span class="auto-style1">Username:</span></strong></td> 
-<td><input type='text' name='user'></td>
-<td><input type='submit' name='submit' value='Log In'></td></tr>
-<tr><td class="auto-style1"><strong>Password:</strong></td> 
-<td><input type='password' name='pass'></td>
-<td><input type='submit' name='register' value='Register'></td>
-</tr>
-</form>
-</table>
+<body>
+	<div id="Container_Normal">
+		<h1 align="center">CyCal</h1>
+		<h1 align="center">Coming Soon</h1>
+	</div>
+	<div id="Container_Normal">
+		<div id="MainContainer">
+			<form method='POST'>
+				<table align="center">
+					<tr>
+						<td>
+							<strong>Username:</strong>
+						</td> 
+						<td>
+							<input type='text' name='user'>
+						</td>
+						<td>
+							<input type='submit' name='submit' value='Log In'>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<strong>Password:</strong>
+						</td> 
+						<td>
+							<input type='password' name='pass'>
+						</td>
+						<td>
+							<input type='submit' name='register' value='Register'>
+						</td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</div>
+	<div id="Container_Normal">
+		<div id="MainContainer">
+			<h2>News / Updates</h2>
+			<p>None now!</p>
+			<?php
+				//Fetch news!
+			?>
+		</div>
+	</div>
+	
+	<div id="Footer">
+		<div id="MainContainer">
+			<p><a href="#">Home</a> | <a href="#">About</a> | <a href="#">Help</a></p>
+		</div>
+	</div>
 
-<?php
-if($_POST['submit']){
-$user = mysql_real_escape_string($_POST['user']);
-$pass = mysql_real_escape_string($_POST['pass']);
-$extract = mysql_query("SELECT * FROM users WHERE username='".$user."' AND password='".md5($pass)."'");
-$numrows = mysql_num_rows($extract);
-if($user != ''){
-	if($numrows == 0){
-	echo "Access Denied! ".mysql_error();
-	echo "  |  SELECT * FROM users WHERE username='".$user."' AND password='".md5($pass)."'";
-	}
-	else{
-	echo"You are In!";
-//	session_start();
-//	$row = mysql_fetch_assoc($extract);
-//	$id = $row['id'];
-//	$write = $row['writeable'];
-//	$_SESSION['id']= $id;
-//		if($write){
-//		header("Location: add_delete.php");
-//		}
-//		else{
-//		header("Location: server_info.php");
-//		}
-	}
-}
-}
-//require("footer.tpl");
-?>
 </body>
 </html>
