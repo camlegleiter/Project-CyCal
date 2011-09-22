15548cc973819c1c265e2b6497c4b66cb00a8847
Justin Derby <justderb@iastate.edu>
2011-09-22 13:54:56 -0500

    Adding salt's

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,14 @@
 
 error_reporting(0);
 
+$globalsalt = '$%hl25d-k1tgbG$bh^';
+
 $connect = mysql_connect("localhost", "root", "localpost") or die(mysql_error());//connect to the sql
 mysql_select_db("cycal") or die("Error: Could not connect to database! Try again later!"); //sql database name
 
+
+function getPassword($username, $password)
+{
+	return sha1($globalsalt.$password.$username);
+}
 ?>
