f0a9e7c2441b155828ace58036007000c3f71535
Jamison Voss <jdvoss@iastate.edu>
2011-09-22 15:18:49 -0500

    Made some changes to about.php

--- about.php
+++ about.php
@@ -3,9 +3,29 @@ $title = "About";
 include ("includes/header.php");
 ?>
 
+<div id="Container_Wide">
 
 
 
+<h1>About</h1> 
+<P>CyCal is a fully customizable event calender for Iowa State University students.  
+We are a group of Software and Computer Engineering students here at Iowa State, and 
+CyCal was a project we decided to attempt for out ComS 319 interfaces class.  
+The purpose of the project is to allow students to view and manipulate the many 
+campus events in an interactive and customizable setting. </p>
+
+<h1>Developers</h1>
+
+<UL>
+<LI>Justin Derby</LI>
+<LI>Michael Kinsella</LI>
+<LI>Kurt Kohl</LI>
+<LI>Blair Billings</LI>
+<LI>Jamison Voss</LI>
+<LI>Cameron Legleiter</LI> 
+</UL>
+
+</div>
 <?php
 include ("includes/footer.php");
 ?>
