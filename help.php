2df72efd61932f94864cee19ba2c998a3ab4c3a9
Cameron <cameron@MacBook.local>
2011-09-22 13:53:32 -0500

    Added expand/collapse features to help page with necessary libraries

--- help.php
+++ help.php
@@ -3,8 +3,37 @@ $title = "Help";
 include ("includes/header.php");
 ?>
 
+<script type="text/javascript" src="js/expand.js"></script>
 
+<script type="text/javascript">
+$(function() {
+	$("h4#headers").toggler({speed: "fast"});
+});
+</script>
 
+<div id="Container_Wide">
+	<h1>Help</h1>
+	<h4 id="headers">Part 1</h4>
+	<div class="collapse">
+		<p class="para">Bacon ipsum dolor sit amet cillum tenderloin in magna, tail nisi filet mignon leberkäse do pork aute et. Tempor adipisicing turkey turducken sint. Irure occaecat elit pork, cow culpa dolore nostrud nulla reprehenderit in ex incididunt sausage cupidatat. In chicken pork, mollit strip steak labore meatloaf ex prosciutto ullamco sirloin brisket est. Dolore turducken pastrami chicken enim tail. Beef consequat andouille, eu anim nulla ball tip in swine cupidatat fugiat. Tenderloin flank occaecat dolor.</p>
+	</div>
+	<h4 id="headers">Part 2</h4>
+	<div class="collapse">
+		<p class="para">Incididunt reprehenderit biltong dolor beef ribs jowl, capicola beef ball tip. Laboris consectetur tail boudin et swine shoulder, dolore rump aute fatback sausage mollit est. Pork belly pork chop pancetta, pariatur chuck laborum fugiat capicola esse ex hamburger. Duis velit flank esse. Shank aliquip flank, ham hock ea beef quis ad shoulder filet mignon meatloaf labore ground round. Exercitation beef leberkäse, non occaecat ex meatloaf hamburger ullamco strip steak ham tempor officia incididunt excepteur. Rump ea ham hock ut sausage, nulla ullamco.</p>
+	</div>
+	<h4 id="headers">Part 3</h4>
+	<div class="collapse">	
+		<p class="para">Strip steak short ribs pork loin laboris magna pig, andouille cupidatat. Tail in shankle, filet mignon fugiat culpa nostrud beef. Ut kielbasa leberkäse elit laborum shank, prosciutto ball tip meatball fugiat ham hock shoulder. Ham hock in ribeye qui sausage biltong. Pastrami eu pariatur, tenderloin t-bone sirloin prosciutto tongue non. Boudin elit ut shoulder excepteur ullamco. Quis prosciutto ad, meatloaf meatball consectetur salami boudin laborum officia labore eiusmod non beef.</p>
+	</div>
+	<h4 id="headers">Part 4</h4>
+	<div class="collapse">		
+		<p class="para">Ullamco t-bone laboris ham hock in cupidatat. Consectetur turkey ex pork loin fugiat meatloaf, mollit minim laborum drumstick. Boudin swine pastrami qui aute in, nisi pork belly enim tri-tip turducken t-bone beef brisket fugiat. Rump strip steak dolore, veniam brisket tempor in consectetur ribeye. Leberkäse tempor eu adipisicing. Pork loin shoulder duis, in nulla brisket ham hock spare ribs nostrud non tri-tip chicken sed aliquip. Hamburger reprehenderit salami et, est t-bone veniam culpa aliquip sirloin pork ex.</p>
+	</div>
+	<h4 id="headers">Part 5</h4>
+	<div class="collapse">		
+		<p class="para">Ea sint proident, aliquip ut ut shankle tempor reprehenderit laborum enim short loin. Labore drumstick magna spare ribs pariatur sint. Dolor esse swine, commodo aliqua magna brisket ullamco anim kielbasa consequat pork belly in tenderloin corned beef. Culpa shankle et, aliqua sausage ham eu ut hamburger swine pork fatback velit in ut. Dolore rump cupidatat jowl short loin, veniam ham shank. Filet mignon veniam aliqua, boudin magna culpa consequat. Eiusmod eu andouille, consequat et adipisicing excepteur enim.</p>
+	</div>
+</div>
 
 <?php
 include ("includes/footer.php");
