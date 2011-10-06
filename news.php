55b5d0b41e609e596bc6285d989f54b3358e4f8b
Justin Derby <justderb@iastate.edu>
2011-10-05 22:06:04 -0500

    Added a test news article.

--- news.php
+++ news.php
@@ -1,7 +1,12 @@
 <div id="Container_Wide">
 	<div id="MainContainer">
 		<h2>News / Updates</h2>
-		<p>None now!</p>
+		<div id="articleTitle">
+			Hear about our launch!
+		</div>
+		<div id="article">
+			Keep checking back to see when we start handing out beta codes for early access/testing!
+		</div>
 		<?php
 			//Fetch news!
 		?>
