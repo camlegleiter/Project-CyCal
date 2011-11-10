080b6e38d50cacfd8bc15762b2797836603a67ee
Cameron <cameron@cameronl.student.iastate.edu>
2011-11-09 22:20:17 -0600

    Moved pipes.php, changed JavaScript to reflect this

--- js/getjson.js
+++ js/getjson.js
@@ -3,7 +3,7 @@ var jsonObject;
 function showRSS(str) {
 	str = encodeURIComponent(str);
 
-	$.get("pipes.php?q=" + str, function(xml, status) {
+	$.get("../includes/pipes.php?q=" + str, function(xml, status) {
 		jsonObject = xml2json.parser(xml);
 	});
 }
