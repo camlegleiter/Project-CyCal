a59ba0bfd04a0535a306dc95cd50e27c77118a49
Justin Derby <justderb@iastate.edu>
2011-11-30 21:30:25 -0600

    Added ISU Feed javascript

--- includes/topBar_header.php
+++ includes/topBar_header.php
@@ -1,2 +1,8 @@
 <link href="css/topbar.css" rel="stylesheet" type="text/css">
 <script src="js/topBar.js" type="text/javascript"></script>
+<script type="text/javascript">
+	function addISUFeed()
+	{
+		window.location.href = 'newuserchoosefeed.php?new=1';
+	}
+</script>
